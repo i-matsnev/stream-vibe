72041f30f70eb5b30e1008a99216ddb2afe57635
i-matsnev <flash.matsnev132@gmail.com>
2025-11-05 16:31:09 +0700

    feat: js логика select

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import SliderCollection from "@/modules/SliderCollection"
 import TabsCollections from "@/modules/TabsCollection"
 import VideoPlayerCollection from "@/modules/VideoPlayerCollection"
 import InputMaskCollection from "@/modules/InputMaskCollections"
+import SelectCollection from "@/modules/SelectCollection"
 
 new OverlayMenu()
 new SliderCollection()
 new TabsCollections()
 new VideoPlayerCollection()
 new InputMaskCollection()
+new SelectCollection()
