fbd2d273fd5bdf3ee62e0f64e8a0a3574ff2bb80
i-matsnev <flash.matsnev132@gmail.com>
2025-11-07 16:04:11 +0700

    feat: секция с таблицей перестраивается в секцию с табами для mobile

--- src/sections/PlansComparison/PlansComparison.jsx
+++ src/sections/PlansComparison/PlansComparison.jsx
@@ -3,6 +3,8 @@ import classNames from 'classnames'
 import Section from "@/layouts/Section"
 import Badge from "@/components/Badge"
 import Table from "@/components/Table"
+import Specifications from "@/components/Specifications"
+import Tabs from "@/components/Tabs"
 
 const PlansComparison = (props) => {
   const {
@@ -17,16 +19,20 @@ const PlansComparison = (props) => {
     {
       children: 'Basic',
       width: '25%',
+      tabsTitle: 'Basic',
     },
     {
       children: <>Standard&nbsp;<Badge mode="accent">Popular</Badge></>,
       width: '25%',
+      tabsTitle: 'Standard',
     },
     {
       children: 'Premium',
       width: '25%',
+      tabsTitle: 'Premium',
     },
   ]
+
   const rows = [
     {
       cells: [
@@ -42,7 +48,8 @@ const PlansComparison = (props) => {
         'Access to a wide selection of movies and shows, including some new releases.',
         'Access to a wider selection of movies and shows, including most new releases and exclusive content',
         'Access to a widest selection of movies and shows, including all new releases and Offline Viewing',
-      ]
+      ],
+      isWide: true,
     },
     {
       cells: [
@@ -50,7 +57,8 @@ const PlansComparison = (props) => {
         'Watch on one device simultaneously',
         'Watch on Two device simultaneously',
         'Watch on Four device simultaneously',
-      ]
+      ],
+      isWide: true,
     },
     {
       cells: [
@@ -110,6 +118,22 @@ const PlansComparison = (props) => {
     },
   ]
 
+  const tabsItems = headCells
+    .filter((headCell) => headCell.tabsTitle)
+    .map((headCell, headCellIndex) => ({
+      title: headCell.tabsTitle,
+      isActive: headCellIndex === 0,
+      children: (
+        <Specifications
+          items={rows.map(({ cells, isWide }, index) => ({
+            key: cells[0],
+            value: cells[headCellIndex + 1],
+            isWide,
+          }))}
+        />
+      )
+    }))
+
   return (
     <Section
       title="Compare our plans and find the right one for you"
@@ -117,8 +141,14 @@ const PlansComparison = (props) => {
       description="StreamVibe offers three different plans to fit your needs: Basic, Standard, and Premium. Compare the features of each plan and choose the one that's right for you."
     >
       <Table
+        className="hidden-mobile"
         headCells={headCells}
         rows={rows}
+      />
+      <Tabs
+        className="visible-mobile"
+        title="plans-comparison-tabs-title"
+        items={tabsItems}
       />
     </Section>
   )
