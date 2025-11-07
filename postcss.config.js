9ef67115b66f6588f102553fbe3f402160b82e7b
i-matsnev <flash.matsnev132@gmail.com>
2025-11-07 17:16:31 +0700

    feat: установил PostCSS-плагин postcss-preset-env, который при билде будет преобразовывать современный синтаксис стилей в более старый, но гарантировано понятный браузеру

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,5 @@
 import postcssPxToRem from 'postcss-pxtorem'
+import postcssPresetEnv from 'postcss-preset-env'
 
 export default ({ env }) => {
   const isProduction = env === 'production'
@@ -10,6 +11,10 @@ export default ({ env }) => {
         propList: ['*'], //Перечисление строк с названиями css свойств, которые будут подвержены PxToRem
         mediaQuery: true,
       })
+    )
+
+    plugins.push(
+      postcssPresetEnv()
     )
   }
   return {
